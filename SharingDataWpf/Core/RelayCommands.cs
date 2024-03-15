bd25594e3f226a14e1ac247b3b39b27f884299b3
Trofim Chuchuk <trofim.chuchuk@mail.ru>
2024-03-16 01:34:12 +0700

    Добавил RelayCommand

---
+++ SharingDataWpf/Core/RelayCommands.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SharingDataWpf.Core {
+    internal class RelayCommands:ICommand {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public RelayCommands(Action<object> Execute, Func<object, bool> CanExecute = null) {
+            _Execute = Execute;
+            _CanExecute = CanExecute;
+        }
+
+        public event EventHandler CanExecuteChanged {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public  bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public  void Execute(object parameter) {
+           _CanExecute?.Invoke(parameter);
+        }
+
+    }
+}
